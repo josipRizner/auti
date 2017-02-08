9fa5f03cddcec3a75644f32c5d41e518aa6ed571
Josip Rizner <josip.rizner@skole.hr>
2017-02-08 12:38:31 +0100

    Postavili smo dizajn.

---
+++ menu_profile.php
@@ -0,0 +1,9 @@
+  <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Dobrodošao,</span>
+                <h2><-To nisam ja, ja sam Josip Rizner</h2>
+              </div>
+            </div>
