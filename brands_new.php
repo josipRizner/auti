66ebd8bff15f3bc7be95e85321033e93410c0e04
Josip Rizner <josip.rizner@skole.hr>
2017-02-14 18:49:52 +0100

    Izmijenili smo naziv iznad forme.

--- brands_new.php
+++ brands_new.php
@@ -41,7 +41,7 @@
           <div class="">
             <div class="page-title">
               <div class="title_left">
-                <h3>Form Validation</h3>
+                <h3>Proizvođači</h3>
               </div>
 
               <div class="title_right">
@@ -61,7 +61,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_content">
-                  <h1>Unos proizvođača</h1>
+                  <h1>Unos</h1>
 
                   <form action="brands_create.php" method="post" class="form-horizontal form-label-left">
                     <div class="form-group">
